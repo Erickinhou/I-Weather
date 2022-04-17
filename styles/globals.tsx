38885fbaf05ed61739d3bb37e567c424803cd622
Erickinhou <willianerick17@gmail.com>
2022-04-17 17:12:18 -0300

    fix: remove browser text style

--- styles/globals.tsx
+++ styles/globals.tsx
@@ -6,6 +6,11 @@ export default createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+  h1, h2, h3, h4, h5, h6, p, blockquote{
+    padding: 0;
+    margin: 0;
+    font-family: "Poppins", sans-serif;
+  }
   * {
     scrollbar-width: thin;
     scrollbar-color: rgba(155, 155, 155, 0.5) transparent;
