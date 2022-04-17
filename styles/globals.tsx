257177abc28c68567c7243efad1b9b92b7344b46
Erickinhou <willianerick17@gmail.com>
2022-04-17 17:11:42 -0300

    feat: add scrollbar style

--- styles/globals.tsx
+++ styles/globals.tsx
@@ -4,6 +4,24 @@ export default createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
+    box-sizing: border-box;
   }
-  
+  * {
+    scrollbar-width: thin;
+    scrollbar-color: rgba(155, 155, 155, 0.5) transparent;
+  }
+
+  *::-webkit-scrollbar {
+    height: 7px;
+  }
+
+  *::-webkit-scrollbar-track {
+    background: transparent;
+  }
+
+  *::-webkit-scrollbar-thumb {
+    background-color: rgba(155, 155, 155, 0.5);
+    border-radius: 20px;
+    border: transparent;
+  }
 `;
