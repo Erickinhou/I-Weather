8ba06530ec3b062301e93ffdee745e525c01aa10
Erickinhou <willianerick17@gmail.com>
2022-04-17 23:25:37 -0300

    feat: add meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,9 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
+const meta = {
+  name: `I-Weather`,
+  description: `It's an app that connects to a public weather and geocoding REST APIs. Display the 7 day forecast for a specified address on an HTML page.`,
+};
 class MyDocument extends Document {
   render() {
     return (
@@ -9,6 +13,14 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Poppins:wght@300;400;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="icon" href="/favicon.ico" />
+          <meta content={meta.description} name="description" />
+          <meta property="og:type" content="website" />
+          <meta property="og:site_name" content={meta.name} />
+          <meta property="og:description" content={meta.description} />
+          <meta property="og:title" content={meta.name} />
+          <meta name="twitter:title" content={meta.name} />
+          <meta name="twitter:description" content={meta.description} />
         </Head>
         <body>
           <Main />
